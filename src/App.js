f1bf3aad49f1986d98a83372cd42ecea5cafb130
Osama Hanhan <hanhan.osama@gmail.com>
2020-12-21 12:50:40 -0800

    log in and registration now both work

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import LeftSideBar from "./components/LeftSideBar/LeftSideBar";
 import styles from "./App.module.css";
 import LoggedInPage from "./pages/LoggedInPage/LoggedInPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LogInPage from "./pages/LogInPage/LogInPage";
 
 function App() {
   return (
     <div className={styles.App}>
-      <RegisterPage />
+      <LogInPage />
     </div>
   );
 }
--- src/pages/LogInPage/LogInPage.js
+++ src/pages/LogInPage/LogInPage.js
@@ -1,15 +1,12 @@
 import { React, useState } from "react";
 
-import styles from "./RegisterPage.module.css";
+import styles from "./LogInPage.module.css";
 
 const axios = require('axios');
 
 
-function RegisterPage() {
+function LogInPage() {
 
-    const [firstName, setFirstName] = useState(false);
-    const [lastName, setLastName] = useState(false);
-    const [email, setEmail] = useState(false);
     const [username, setUsername] = useState(false);
     const [password, setPassword] = useState(false);
     const [errorMessage, setErrorMessage] = useState(false);
@@ -21,28 +18,25 @@ function RegisterPage() {
      */
     const handleSubmit = () => {
 
-        if (firstName && lastName && email && username && password) {
+        if (username && password) {
 
-            axios.post("http://localhost:5000/user/userRegister", {
+            axios.post("http://localhost:5000/user/userLogin", {
                 username: username,
-                password: password,
-                email: email,
-                firstName: firstName,
-                lastName: lastName
+                password: password
 
             }).then((response) => {
-                setErrorMessage(response.data);
-                document.getElementById("register-form").reset();
+                console.log(response.data.userUUID);
+                setErrorMessage("logged");
+                document.getElementById("login-form").reset();
 
             }).catch((error) => {
                 setErrorMessage("Invalid input!");
+                document.getElementById("login-form").reset();
+
 
             });
 
-            //reset state variables
-            setFirstName(false);
-            setLastName(false);
-            setEmail(false);
+
             setUsername(false);
             setPassword(false);
 
@@ -61,39 +55,21 @@ function RegisterPage() {
 
 
                     <button className={styles.Buttons}>
-                        Log In Page
+                        Register Page
                     </button>
                 </div>
 
 
 
-                <div className={styles.RegisterContainer}>
+                <div className={styles.LoginContainer}>
 
-                    <form id="register-form" className={styles.RegisterForm} >
+                    <form id="login-form" className={styles.LoginForm} >
 
                         <div className={styles.error}>
                             {errorMessage && <div >{errorMessage}</div>}
 
                         </div>
 
-                        <div className={styles.FormSection}>
-
-                            <div>
-                                <div>First Name *</div>
-                                <input onChange={(e) => {
-                                    setFirstName(e.target.value);
-                                }} name="first_name"></input>
-                            </div>
-
-                            <div>
-                                <div>Last Name *</div>
-                                <input onChange={(e) => setLastName(e.target.value)} name="last_name"></input>
-                            </div>
-                            <div>
-                                <div>Email *</div>
-                                <input type="email" onChange={(e) => setEmail(e.target.value)} name="email"></input>
-                            </div>
-                        </div>
 
                         <div className={styles.FormSection}>
                             <div>
@@ -105,9 +81,6 @@ function RegisterPage() {
                                 <div>Password *</div>
                                 <input onChange={(e) => setPassword(e.target.value)} name="password"></input>
                             </div>
-
-
-
                         </div>
 
 
@@ -131,4 +104,4 @@ function RegisterPage() {
     );
 }
 
-export default RegisterPage;
+export default LogInPage;
